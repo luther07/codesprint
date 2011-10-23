eff3a2cd95ad578a56bb5a6e598bcc68b09c59dc
Mark Johnson <luther07@gmail.com>
2011-10-23 14:05:12 -0500

    changed variable names

--- twoscomplement/Solution.java
+++ twoscomplement/Solution.java
@@ -1,48 +1,71 @@
 import java.io.*;
+import java.util.ArrayList;
 public class Solution {
+
+  public int factoril( int num ) {
+    int result = 1;
+    for( int i = 1; i <= num; i++) {
+      result = i *  result;
+    }
+    return result;
+  }
   public static void main(String [] args){
     int trials = 0;
     String s;
-    String trialData = new String();
-    String first = new String();
-    String rest = new String();
-    int indx = 0;
-    int a;
-    int b;
-    int ones;
-    int runningTotal;
+    ArrayList<Integer> trialInfo = new ArrayList<Integer>();
+    int range1, range2;
+    long runningTotal;
+    int tricky1, tricky2;
+    double topDifference = 0;
 
     try{
        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
        s = br.readLine();
        trials = Integer.parseInt(s);
-       System.out.println("Number is: " + trials);
        for(int i = 1; i <= trials; i++) {
          s = br.readLine();
-         trialData = trialData + s + "\t";
+         range1 = Integer.parseInt(s.substring(0, s.indexOf(32)));
+         range2 = Integer.parseInt(s.substring(s.indexOf(32) + 1));
+         trialInfo.add(new Integer(range1));
+         trialInfo.add(new Integer(range2));
        }
     } catch (Exception e) {
         System.err.println("Error: " + e.getMessage());
       }
 
+    //loop for number of trials
     for(int t = 1; t<= trials; t++){
       runningTotal = 0;
-      first = trialData.substring(0, trialData.indexOf(9));
-      rest = trialData.substring(trialData.indexOf(9)+1);
-      trialData = rest;
-      a = Integer.parseInt(first.substring(0, first.indexOf(32)));
-      b = Integer.parseInt(first.substring(first.indexOf(32)+1));
+      range1 = trialInfo.get(2*t-2);
+      range2 = trialInfo.get(2*t-1);
 
-      for(int startnum = a; startnum <= b; startnum++) {
-        ones = 0;
-        for(int z=1, myint = startnum; z<=32; z++){
-          if(myint == 0) {
-            runningTotal = runningTotal + ones;
+      if (d>(Math.abs(range1)*1000)) {
+        for(int i = 1; i <= 31; i++) {
+          if (range2 < (Math.pow(2, i))) {
+            topDifference = Math.pow(2, i-1);
             break;
           }
-          if(myint%2 == 1) ones++;
-          myint = myint/2;
         }
+        //section where I need to count how many ways to have one 1, two ones, etc and multiply
+      //  for(int i = 1; i <= 31; i++)
+      //    runningTotal = runningTotal +  
+      }
+
+      if ((range1<0) && (range2>=0) && (Math.abs(range1)>=Math.abs(range2+1))) {
+        runningTotal = runningTotal + (32 * (range2+1));
+        tricky1 = range1;
+        tricky2 = (-range2) - 2;
+      } else if ((range1<0) && (range2>=0) && (Math.abs(range1)<Math.abs(range2))) {
+        runningTotal = runningTotal + (32 * (Math.abs(range1)));
+        tricky1 = (-range1);
+        tricky2 = range2;
+      } else {
+          tricky1 = range1;
+          tricky2 = range2;
+      }
+
+      for(int startnum = tricky1; startnum <= tricky2; startnum++) {
+        runningTotal = runningTotal + Integer.bitCount(startnum);
       }
       System.out.println(runningTotal);
     } 
